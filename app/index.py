2008d835fc6075560812845f4fbbaac61a8a0b53
DennisMuasyaWambua <muasyathegreat4@gmail.com>
2023-08-31 10:04:06 +0300

    path updated

--- app/index.py
+++ app/index.py
@@ -36,15 +36,15 @@ def preprocess_and_model(data:Soil):
     rainfall = data.rainfall
     data = [[N, P, K, temperature, humidity, ph, rainfall]]
     df = pd.DataFrame(data, columns=['N', 'P', 'K', 'temperature', 'humidity', 'ph', 'rainfall'])
-    full_pipeline = joblib.load('/home/ubuntu/farmerproj/app/saved_model/02-05-2023_20-47-39_full_pipeline.pkl')
-    xgb_clf = joblib.load('/home/ubuntu/farmerproj/app/saved_model/02-05-2023_20-47-39_xgb_clf.pkl')
+    full_pipeline = joblib.load('/home/ubuntu/farmerproj/farmer/app/saved_model/02-05-2023_20-47-39_full_pipeline.pkl')
+    xgb_clf = joblib.load('/home/ubuntu/farmerproj/farmer/app/saved_model/02-05-2023_20-47-39_xgb_clf.pkl')
 
 
     prepared_data = full_pipeline.transform(df)
     prediction = xgb_clf.predict(prepared_data)
 
    
-    target_encoder = joblib.load('/home/ubuntu/farmerproj/app/saved_model/02-05-2023_20-47-39_target_encoder.pkl')
+    target_encoder = joblib.load('/home/ubuntu/farmerproj/farmer/app/saved_model/02-05-2023_20-47-39_target_encoder.pkl')
 
     target_value = target_encoder.inverse_transform(prediction)
 
